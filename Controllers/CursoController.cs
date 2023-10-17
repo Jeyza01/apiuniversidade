52df086b05d2ca6301d58287abaa634509fe3e6c
Jeyza <santana.jeyza@escolar.ifrn.edu.br>
2023-10-17 11:17:23 -0300

    aula 17.10

--- Controllers/CursoController.cs
+++ Controllers/CursoController.cs
@@ -3,21 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using apiUniversidade.Model;
+using apiUniversidade.Context;
 using Microsoft.AspNetCore.Mvc;
 
 namespace apiUniversidade.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class CursoController : ControllerBase
+    public class CursoController : Controller
     {
-        private readonly (ILogger<CursoController> logger, Apiuniversidade context)
-        private readonly apiUniversidadeContext_context;
+        private readonly ILogger<CursoController> _logger;
+        private readonly ApiUniversidadeContext _context; 
+
+        public CursoController (ILogger<CursoController> logger, ApiUniversidadeContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
         
         [HttpGet]
         public ActionResult<IEnumerable<Curso>> Get()
         {
-            var cursos = context.Cursos.ToList();
+            var cursos = _context.Cursos.ToList();
             if(cursos is null)
                 return NotFound();
             
